9bf4bd44b43544180f6859d5d60d6e3057e2a2be
EVEnMore <143513377+EVEnMoregachi@users.noreply.github.com>
2025-07-11 16:12:15 +0800

    完成Bundle构建工具

---
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildTool : Editor
+{
+    [MenuItem("Tools/Build Windows Bundle")]
+    static void BuildWindowsBundle()
+    {
+        Build(BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Tools/Build Android Bundle")]
+    static void BuildAndroidBundle()
+    {
+        Build(BuildTarget.Android);
+    }
+    [MenuItem("Tools/Build iOS Bundle")]
+    static void BuildBundle()
+    {
+        Build(BuildTarget.iOS);
+    }
+
+    static void Build(BuildTarget target)
+    {
+        List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+        // 按文件打包(GetFiles)
+        string[] files = Directory.GetFiles(PathUtil.BuildResoursesPath, "*", SearchOption.AllDirectories);
+
+        for (int i = 0; i < files.Length; i++)
+        {   // 排除meta文件
+            if (files[i].EndsWith(".meta")) continue;
+
+            string filename = PathUtil.GetStandardPath(files[i]);
+            Debug.Log("file:" + filename);
+            AssetBundleBuild assetBundle = new AssetBundleBuild();
+            string assetName = PathUtil.GetUnityPath(filename);
+            assetBundle.assetNames = new string[] { assetName };
+            string bundleName = filename.Replace(PathUtil.BuildResoursesPath, "").ToLower();
+            assetBundle.assetBundleName = bundleName + ".ab";
+            assetBundleBuilds.Add(assetBundle);
+        }
+
+        if (Directory.Exists(PathUtil.BundleOutPath))
+            Directory.Delete(PathUtil.BundleOutPath, true);
+        Directory.CreateDirectory(PathUtil.BundleOutPath);
+
+        BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.None, target);
+    }
+}
+
---
+++ Assets/Scripts/Framework/Util/PathUtil.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathUtil
+{
+    // 如果每次打包都使用Application.dataPath，会产生GC，这里定义后只需要访问一次
+    // 根目录
+    public static readonly string AssetsPath = Application.dataPath;
+    // 需要打Bundle的目录
+    public static readonly string BuildResoursesPath = AssetsPath + "/BuildResources/";
+    // Bundle 输出目录
+    public static readonly string BundleOutPath = Application.streamingAssetsPath;
+
+    /// <summary>
+    /// 获取Unity的相对路径
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string GetUnityPath(string path)
+    {
+        if (string.IsNullOrEmpty(path)) 
+            return string.Empty;
+        return path.Substring(path.IndexOf("Assets"));// 截取Assets之后的路径
+    }
+
+    /// <summary>
+    /// 获取标准路径
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string GetStandardPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            return string.Empty;
+        return path.Trim().Replace("\\", "/");
+    }
+}
